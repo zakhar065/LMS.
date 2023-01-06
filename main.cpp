a4fde14653df40442f1ee34cd190abd073ccd3c2
zakhar065 <115736138+zakhar065@users.noreply.github.com>
2023-01-06 15:18:09 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,25 +1,18 @@
 #include <stdio.h>
-
-main()
+/*пароль*/
+main() 
 {
-	int a,b;
-	printf("a=");
-	scanf("%i",&a);
-	printf("b=");
-	scanf("%i",&b);
-	if(a>b)
+	int pin1,pin2;
+	printf("pin1=");
+	scanf("%i",&pin1);
+	printf("pin2=");
+	scanf("%i",&pin2);
+	if(((pin1==111)&&(pin2==222))||((pin1==888)&&(pin2==000)))
 	{
-		printf("a bolshe");
+		printf("ok");
 	}
 	else
 	{
-		if(a==b)
-		{
-			printf("a=b");
-		}
-		else
-		{
-			printf("b bolshe");
-		}
+		printf("error");
 	}
 }
